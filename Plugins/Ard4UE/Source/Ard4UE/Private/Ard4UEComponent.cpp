989e8ff335ed9e7afa5dc6b15368c08a0d2657df
Litruv <litruv@gmail.com>
2017-12-07 18:46:04 +1100

    Added Arduino Events and Autoconnect

--- Plugins/Ard4UE/Source/Ard4UE/Private/Ard4UEComponent.cpp
+++ Plugins/Ard4UE/Source/Ard4UE/Private/Ard4UEComponent.cpp
@@ -1,17 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "Ard4UEComponent.h"
 #include "Ard4UE.h"
 #include "engine.h"
 
 
-using std::vector;
 serial::Serial mySerial;
+
 
 void UArd4UEComponent::enumeratePorts(TArray<FString> &ComPorts) const
 {
-	vector<serial::PortInfo> devices_found = serial::list_ports();
-	vector<serial::PortInfo>::iterator iter = devices_found.begin();
+	std::vector<serial::PortInfo> devices_found = serial::list_ports();
+	std::vector<serial::PortInfo>::iterator iter = devices_found.begin();
 
 	while (iter != devices_found.end())
 	{
@@ -29,14 +27,88 @@ void UArd4UEComponent::disconnectPort() const
 	}
 }
 
-void UArd4UEComponent::initializeConnection(FName COMPort, int BAUDRate /*= 9600*/, int bytesize /*= 8*/) const
+void UArd4UEComponent::initializeConnection(FName COMPort, int BAUDRate /*= 9600*/) const
 {
 	mySerial.setPort(std::string(TCHAR_TO_UTF8(*COMPort.ToString())));
 	mySerial.setBaudrate(BAUDRate);
 
-	mySerial.setBytesize(serial::eightbits);
+	//Try Catch to avoid in use devices
+	try {
+		mySerial.open();
+	}
+	catch (const std::exception&)
+	{
 
-	mySerial.open();
+	}
+}
+
+void UArd4UEComponent::sendCommand(FString command)
+{
+	if (mySerial.isOpen())
+		mySerial.write(TCHAR_TO_UTF8(*command));
+}
+
+void UArd4UEComponent::autoConnect(FString controllerID, int Baud, bool &Connected)
+{
+	if (mySerial.isOpen())
+	{
+		//Try Catch to avoid in use devices
+		try {
+			disconnectPort();
+		}
+		catch (const std::exception&)
+		{
+
+		}
+	}
+
+	Connected = false;
+
+	TArray<FString> portCollection;
+	enumeratePorts(portCollection);
+
+	for (FString portName : portCollection)
+	{
+		initializeConnection(FName(*portName), Baud);
+		if (mySerial.isOpen())
+		{
+			sendCommand("id");
+			std::string stringRead;
+
+			//100ms per check per device (20*device count)(seems reliable)
+			mySerial.setTimeout(serial::Timeout::max(), 100, 0, 100, 0);
+
+			for (int i = 0; i < 20; i++)
+			{
+				mySerial.readline(stringRead);
+
+				//Look for the 'i'nformation character
+				if (FString(stringRead.c_str()).StartsWith("i"))
+				{
+					break;
+				}
+
+				//clear stringRead for next loop.
+				stringRead.clear();
+			}
+
+			//Clean up and compare strings.
+			FString stringConvert(stringRead.c_str());
+			stringConvert.RemoveFromEnd("\r\n");
+			stringConvert.RemoveFromStart("i");
+
+			if (stringConvert.Equals(controllerID))
+			{
+				Connected = true;
+				break;
+			}
+
+			//disconnect and check the next one
+			disconnectPort();
+		}
+	}
+
+
 }
 
 UArd4UEComponent::UArd4UEComponent()
@@ -45,23 +117,11 @@ UArd4UEComponent::UArd4UEComponent()
 
 }
 
-
-void UArd4UEComponent::BeginPlay()
-{
-	Super::BeginPlay();
-
-	// ...
-	
-}
-
-
-// Called every frame
 void UArd4UEComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	if (mySerial.isOpen())
-	{
 		if (mySerial.available() > 1)
 		{
 			std::string stringRead;
@@ -70,28 +130,78 @@ void UArd4UEComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActo
 				FString stringReadFS(stringRead.c_str());
 				FString trimmed = stringReadFS.RightChop(1);
 
+				trimmed.RemoveFromEnd("\r\n");
+
+				//Float Broadcast
 				if (stringReadFS.StartsWith(FString("f")))
 				{
 					OnSerialFloatRecieved.Broadcast(FCString::Atof(*trimmed));
 				}
+				//String Broadcast
 				else if (stringReadFS.StartsWith(FString("s")))
 				{
 					OnSerialStringRecieved.Broadcast(trimmed.ReplaceEscapedCharWithChar());
 				}
-					
+				//Event Broadcast
+				else if (stringReadFS.StartsWith(FString("e")))
+				{
+					FString leftparam, rightparam;
+
+					trimmed.Split(",", &leftparam, &rightparam);
+
+					int eventNum = FCString::Atoi(*leftparam);
+					int paramNum = FCString::Atoi(*rightparam);
+
+					switch (eventNum)
+					{
+					case 1:
+						OnEvent1Recieved.Broadcast(paramNum);
+						break;
+					case 2:
+						OnEvent2Recieved.Broadcast(paramNum);
+						break;
+					case 3:
+						OnEvent3Recieved.Broadcast(paramNum);
+						break;
+					case 4:
+						OnEvent4Recieved.Broadcast(paramNum);
+						break;
+					case 5:
+						OnEvent5Recieved.Broadcast(paramNum);
+						break;
+					case 6:
+						OnEvent6Recieved.Broadcast(paramNum);
+						break;
+					case 7:
+						OnEvent7Recieved.Broadcast(paramNum);
+						break;
+					case 8:
+						OnEvent8Recieved.Broadcast(paramNum);
+						break;
+					case 9:
+						OnEvent9Recieved.Broadcast(paramNum);
+						break;
+					case 10:
+						OnEvent10Recieved.Broadcast(paramNum);
+						break;
+
+					default:
+						break;
+					}
+
+				}
+
 			}
 		}
-	}
-
-
 }
 
 void UArd4UEComponent::BeginDestroy()
 {
-
 	Super::BeginDestroy();
 
-		if ( mySerial.isOpen())
+	//Kill connection on object destroy
+	if (mySerial.isOpen())
 		UArd4UEComponent::disconnectPort();
+
 }
 
--- Plugins/Ard4UE/Source/Ard4UE/Public/Ard4UEComponent.h
+++ Plugins/Ard4UE/Source/Ard4UE/Public/Ard4UEComponent.h
@@ -11,33 +11,131 @@
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMessageFloatDelegate, float, Number);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMessageStringDelegate, FString, String);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMessageEventDelegate, int32, IntParam);
 
-UCLASS(Blueprintable, BlueprintType, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(Blueprintable, BlueprintType, ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class ARD4UE_API UArd4UEComponent : public UActorComponent
 {
 	GENERATED_BODY()
 		UFUNCTION(BlueprintPure, meta = (DisplayName = "Enumerate Ports", Keywords = "Ard4UE enumerate ports arduino"), Category = "Ard4UE")
+		//************************************
+		// Returns A list of COM Ports
+		// Method:    enumeratePorts
+		// FullName:  UArd4UEComponent::enumeratePorts
+		// Access:    private 
+		// Returns:   void
+		// Qualifier: const
+		// Parameter: TArray<FString> & ComPorts
+		//************************************
 		void enumeratePorts(TArray<FString> &ComPorts) const;
+	
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Disconnect ComPort", Keywords = "Ard4UE disconnect arduino"), Category = "Ard4UE")
+		//************************************
+		// Disconnect the current COM Port
+		// Method:    disconnectPort
+		// FullName:  UArd4UEComponent::disconnectPort
+		// Access:    private 
+		// Returns:   void
+		// Qualifier: const
+		//************************************
 		void disconnectPort() const;
+	
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Initialize Connection", Keywords = "Ard4UE initialize arduino"), Category = "Ard4UE")
-		void initializeConnection(FName COMPort, int BAUDRate = 9600, int bytesize = 8) const;
+		//************************************
+		// Connect to a port (Will disconnect previous one)
+		// Method:    initializeConnection
+		// FullName:  UArd4UEComponent::initializeConnection
+		// Access:    private 
+		// Returns:   void
+		// Qualifier: const
+		// Parameter: FName COMPort
+		// Parameter: int BAUDRate
+		//************************************
+		void initializeConnection(FName COMPort, int BAUDRate = 9600) const;
+	
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Auto Connect", Keywords = "Ard4UE auto connect arduino"), Category = "Ard4UE")
+		//************************************
+		// Connect to a COM port with the specified device ID
+		// Method:    autoConnect
+		// FullName:  UArd4UEComponent::autoConnect
+		// Access:    private 
+		// Returns:   void
+		// Qualifier:
+		// Parameter: FString controllerID
+		// Parameter: int Baud
+		// Parameter: bool & Connected
+		//************************************
+		void autoConnect(FString controllerID, int Baud, bool &Connected);
+	
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Send Command", Keywords = "Ard4UE send command arduino"), Category = "Ard4UE")
+		//************************************
+		// Send command via serial
+		// Method:    sendCommand
+		// FullName:  UArd4UEComponent::sendCommand
+		// Access:    private 
+		// Returns:   void
+		// Qualifier:
+		// Parameter: FString command
+		//************************************
+		void sendCommand(FString command);
+
 
 
 	UPROPERTY(BlueprintAssignable, Category = "Ard4UE")
+	// Float Received delegate
 		FMessageFloatDelegate OnSerialFloatRecieved;
+	
+	UPROPERTY(BlueprintAssignable, Category = "Ard4UE")
+		// String Received delegate
+		FMessageStringDelegate OnSerialStringRecieved;
 
 	UPROPERTY(BlueprintAssignable, Category = "Ard4UE")
-		FMessageStringDelegate OnSerialStringRecieved;
-public:	
+		// Event 1
+		FMessageEventDelegate OnEvent1Recieved;
+
+	UPROPERTY(BlueprintAssignable, Category = "Ard4UE")
+		// Event 2
+		FMessageEventDelegate OnEvent2Recieved;
+
+	UPROPERTY(BlueprintAssignable, Category = "Ard4UE")
+		// Event 3
+		FMessageEventDelegate OnEvent3Recieved;
+
+	UPROPERTY(BlueprintAssignable, Category = "Ard4UE")
+		// Event 4
+		FMessageEventDelegate OnEvent4Recieved;
+
+	UPROPERTY(BlueprintAssignable, Category = "Ard4UE")
+		// Event 5
+		FMessageEventDelegate OnEvent5Recieved;
+
+	UPROPERTY(BlueprintAssignable, Category = "Ard4UE")
+		// Event 6
+		FMessageEventDelegate OnEvent6Recieved;
+
+	UPROPERTY(BlueprintAssignable, Category = "Ard4UE")
+		// Event 7
+		FMessageEventDelegate OnEvent7Recieved;
+
+	UPROPERTY(BlueprintAssignable, Category = "Ard4UE")
+		// Event 8
+		FMessageEventDelegate OnEvent8Recieved;
+
+	UPROPERTY(BlueprintAssignable, Category = "Ard4UE")
+		// Event 9
+		FMessageEventDelegate OnEvent9Recieved;
+
+	UPROPERTY(BlueprintAssignable, Category = "Ard4UE")
+		// Event 10
+		FMessageEventDelegate OnEvent10Recieved;
+
+public:
 	// Sets default values for this component's properties
 	UArd4UEComponent();
 
 protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 public:
